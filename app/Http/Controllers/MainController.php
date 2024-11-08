95753d4f353d822e902eabf5f5b1172a8630a460
Walter Rodrigues Junior <walter.junior00@pm.pr.gov.br>
2024-11-08 13:30:33 -0300

    atualizando

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        echo "index";
+    }
+
+    public function about()
+    {
+        echo "about";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/','home');
+
+Route::get('/index', [MainController::class, 'index']);
+Route::get('/about', [MainController::class, 'about']);
