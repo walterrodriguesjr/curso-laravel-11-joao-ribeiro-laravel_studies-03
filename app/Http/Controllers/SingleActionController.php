76e342b98d45b0fd36a3a5b1640da9ed01aeca6e
walterrodriguesjr <walterrjr.86@gmail.com>
2024-11-13 14:37:37 -0300

    atualizando

---
+++ app/Http/Controllers/SingleActionController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SingleActionController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request): void
+    {
+        echo "Single action controller";
+        echo "<br>";
+        echo $this->privateMethod();
+    }
+
+    private function privateMethod(): string
+    {
+        return "Private Method";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\ClientsController;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\SingleActionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/init', [MainController::class, 'initMethod'])->name('init');
 Route::get('/view', [MainController::class, 'viewPage'])->name('view');
+
+Route::get('/single', SingleActionController::class)->name('single');
+
+Route::resource('users', UserController::class);
+
+Route::resources([
+    'clients' => ClientsController::class,
+    'products' => ProductsController::class
+]);
