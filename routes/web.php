5140147528f2a9f9e5a05c19bb0b8deb80d590be
walterrodriguesjr <walterrjr.86@gmail.com>
2024-11-13 15:10:08 -0300

    atualizando

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
-use App\Http\Controllers\ClientsController;
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\ProductsController;
-use App\Http\Controllers\SingleActionController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/init', [MainController::class, 'initMethod'])->name('init');
-Route::get('/view', [MainController::class, 'viewPage'])->name('view');
 
-Route::get('/single', SingleActionController::class)->name('single');
-
-Route::resource('users', UserController::class);
-
-Route::resources([
-    'clients' => ClientsController::class,
-    'products' => ProductsController::class
-]);
+Route::get('/admin', [AdminController::class, 'admin'])->name('admin');
