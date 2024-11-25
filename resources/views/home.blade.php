92b9ffcee3cd12019424dffe4d8ef8ed8a859b4b
Walter Rodrigues Junior <walter.junior00@pm.pr.gov.br>
2024-11-25 13:27:05 -0300

    atualizando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.main_layout')
 
 @section('content')
-    <div class="text-center">
+    {{-- <div class="text-center">
         @foreach ($pessoas_linguas as $pessoa => $linguas)
             <x-card-pessoa :$pessoa :$linguas />
         @endforeach
+    </div> --}}
+
+    {{-- componentes e slots --}}
+    <div>
+        <h4 class="text-info">Como funciona o slot</h4>
+        <x-other-card>
+            <h1 class="text-danger">Este é o slot!</h1>
+        </x-other-card>
     </div>
 @endsection
